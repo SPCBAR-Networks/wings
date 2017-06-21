743903027de67d4b1975849ec4b66867a8839956
Michael (Parker) Parker <parkervcp@gmail.com>
2017-06-21 12:37:41 -0400

    stealing from schrej

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pterodactyld
 A new generation of the Pterodactyl daemon, written in go
+
+
+This is my branch and it may be dishevled and disorganized to some. I am mainly testing features and cobbling together stuff to get a basic version working.
+
+Please ignore this branch as it is probably a horrible experience to run.
--- config.go
+++ config.go
@@ -1,74 +1,92 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
+	"github.com/spf13/viper"
 )
 
-//daemonConfig struct
-type daemonConfig struct {
-	//web settings
-	web     string `json:"web"`
-	webip   string `json:"host"`
-	webport int    `json:"listen"`
-	//ssl settings
-	ssl     string `json:"ssl"`
-	sslenbl string `json:"enabled"`
-	sslcert string `json:"certificate"`
-	sslkey  string `json:"key"`
-	//Docker config struct
-	dckr     string `json:"docker"`
-	dckrsckt string `json:"socket"`
-	dckritfc string `json:"interface"`
-	dckrupdt string `json:"autoupdate_images"`
-	dckrtzon string `json:"timezone_path"`
-	//sftp server struct
-	sftp     string `json:"sftp"`
-	sftppath string `json:"path"`
-	sftpport int    `json:"port"`
-	//container query struct
-	query    string `json:"query"`
-	killfail string `json:"kill_on_fail"`
-	faillimt string `json:"fail_limit"`
-	//logger struct
-	logger  string `json:"logger"`
-	logpath string `json:"path"`
-	logsrc  string `json:"src"`
-	loglevl string `json:"level"`
-	logperd string `json:"period"`
-	logcnt  int    `json:"count"`
-	//remote panel struct
-	remote    string `json:"remote"`
-	remtbase  string `json:"base"`
-	remtdnld  string `json:"download"`
-	remtinstl string `json:"installed"`
-	//uploads struct
-	upld      string `json:"uploads"`
-	upldmax   string `json:"maximumSize"`
-	upldlimit string `json:"size_limit"`
-	//keys struct
-	keys []string `json:"keys"`
+// Config contains the configuration of the Pterodactyl Daemon
+type Config struct {
+
+	// Web contains the settings of the api webserver
+	Web struct {
+		// ListenAddress is the address to bind the api webserver to
+		ListenAddress string `json:"address"`
+		// ListenPort is the port to bind the api webserver to
+		ListenPort int16 `json:"port"`
+
+		// SSL contains https configuration for the api webserver
+		SSL struct {
+			// Enabled allows to enable or disable ssl
+			Enabled bool `json:"enabled"`
+			// GenerateLetsEncrypt
+			GenerateLetsEncrypt bool `json:"GenerateLetsEncrypt"`
+			// Certificate is the certificate file path to use
+			Certificate string `json:"certificate"`
+			// Key is the path to the private key for the certificate
+			Key string `json:"key"`
+		} `json:"ssl"`
+
+		// Uploads contains file upload configuration
+		Uploads struct {
+			// MaximumSize is the maximum file upload size
+			MaximumSize int64 `json:"maximumSize"`
+		} `json:"uploads"`
+	} `json:"web"`
+
+	// Docker contains docker related configuration
+	Docker struct {
+		// Socket is the path to the docker control socket
+		Socket string `json:"socket"`
+		// AutoupdateImages allows to disable automatic Image updates
+		AutoupdateImages bool `json:"autoupdateImages"`
+		// NetworkInterface is the interface for the pterodactyl network
+		NetworkInterface string `json:"networkInterface"`
+		// TimezonePath is the path to the timezone file to mount in the containers
+		TimezonePath string `json:"timezonePath"`
+	} `json:"docker"`
+
+	// Sftp contains information on the integrated sftp server
+	Sftp struct {
+		// Path is the base path of the sftp server
+		Path string `json:"path"`
+		// Port is the port to bind the sftp server to
+		Port int16 `json:"port"`
+	} `json:"sftp"`
+
+	// Query contains parameters related to queriying of running gameservers
+	Query struct {
+		KillOnFail bool `json:"killOnFail"`
+		FailLimit  bool `json:"failLimit"`
+	} `json:"query"`
+
+	// Remote is the url of the panel
+	Remote string `json:"remote"`
+
+	// Log contains configuration related to logging
+	Log struct {
+		// Path is the folder where logfiles should be stored
+		Path string `json:"path"`
+		// Level is the preferred log level
+		Level string `json:"level"`
+
+		// DeleteAfterDays is the time in days after which logfiles are deleted
+		// If set to <= 0 logs are kept forever
+		DeleteAfterDays int `json:"deleteAfterDays"`
+	} `json:"log"`
 }
 
-//serverConfig struct
-type serverConfig struct {
-}
-
-func getDaemonConfig(in string) string {
-	//Opens config.json and returns values
-	file, _ := os.Open("config.json")
-	decoder := json.NewDecoder(file)
-	daemon := daemonConfig{}
-
-	err := decoder.Decode(&daemon)
-	if err != nil {
-		log.Fatal("issue decoding the config file", err)
+// LoadConfiguration loads the configuration from the disk.
+func LoadConfiguration() error {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	// Find and read the config file
+	if err := viper.ReadInConfig(); err != nil {
+		return err
 	}
 
-	out := daemon.dckrsckt
-	return out
+	return nil
 }
 
-func getServerConfig() {
+func setDefaults() {
 
 }
